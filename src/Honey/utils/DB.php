925ddb3c12021daee0a5b21bd0d9f1ab30367f34
towa0131 <towa.towa0131@icloud.com>
2017-10-19 01:24:13 +0900

    DB.phpの仕様変更とエラー修正

--- src/Honey/utils/DB.php
+++ src/Honey/utils/DB.php
@@ -2,25 +2,24 @@
 
 namespace Honey\utils;
 
+use pocketmine\Server;
+
+use pocketmine\utils\MainLogger;
+use pocketmine\utils\Config;
+
 use Honey\Main;
 
 class DB{
 
 	/**
 	*	$cache[0]にはmysqliインスタンス(connectDBが呼び出され後)
-	*	$cache[1]にはアドレフ
+	*	$cache[1]にはアドレス
 	*	$cache[2]にはユーザー名
 	*	$cache[3]にはパスワード
 	*	$cache[4]にはデータベース名
 	*	$cache[5]にはタイムアウト時間がはいっている
 	*/
-	private static $cache = [null,
-						Main::getInstance()->config->getNested("DB.address"),
-						Main::getInstance()->config->getNested("DB.user"),
-						Main::getInstance()->config->getNested("DB.password"),
-						Main::getInstance()->config->getNested("DB.database"),
-						Main::getInstance()->config->getNested("DB.timeout")
-						];
+	private static $cache = [null];
 
 	public static function getDB(){
 		if(self::$cache[0] == null){ //キャッシュにmysqliインスタンスがなければデータベースに接続
@@ -33,12 +32,21 @@ class DB{
 		$mysqli = new \mysqli(self::$cache[1], self::$cache[2], self::$cache[3], self::$cache[4]);
 		$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, self::$cache[5]);
 		if($mysqli->connect_errno){
+			MainLogger::getLogger()->error("§a[はにー]§4DBへの接続時にエラーが発生しました。");
+			MainLogger::getLogger()->error("§a[はにー]§4エラーメッセージ : " . $mysqli->connect_error);
 			return null; //接続時にエラー発生
 		}
+		MainLogger::getLogger()->info("§a[はにー]§bDBへの接続に成功しました。");
 		return $mysqli;
 	}
 
 	public static function resetConnect(){
 		self::$cache[0] = null; //$cacheに格納されているmysqliインスタンスを初期化
+	}
+
+	public static function setConfig(...$args){
+		foreach($args as $a){
+			self::$cache[] = $a;
+		}
 	}
 }
